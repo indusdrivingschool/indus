8b0c76c62969402c25521e7f91e763dafbc8cfbc
indusdrivingschool <indusdrivingschoolau@gmail.com>
2026-04-23 11:46:04 -0800

    Update vite.config.ts

--- artifacts/indus-driving-school/vite.config.ts
+++ artifacts/indus-driving-school/vite.config.ts
@@ -5,10 +5,10 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 const port = Number(process.env.PORT) || 5173;
-const basePath = process.env.BASE_PATH || "/";
+
 
 export default defineConfig({
-  base: basePath,
+base: "./",
 
   plugins: [
     react(),
