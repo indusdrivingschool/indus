656c6b77728baa7c2a89a5a0c8f89eaa5f3cd5cb
indusdrivingschool <indusdrivingschoolau@gmail.com>
2026-04-25 02:27:05 -0800

    Update main.tsx

--- artifacts/indus-driving-school/src/main.tsx
+++ artifacts/indus-driving-school/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <h1 style={{ color: "red" }}>WORKING ✅</h1>
+);
